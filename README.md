1977c57204ce9317ed17103b23fd9c017b72b1b9
Mitch Ainslie Galatcha <mitchainslie_galatcha@trendmicro.com>
2019-08-05 12:56:34 +0800

    Updates listdir.py for Machine Problem: listdir with hashes

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 ## listdir.py
-Generates a CSV file  of all files within a directory or folder, these includes:
-- Full directory of a file
-- The name of the file
-- The size of the file
+Generates a file that list all the files within a directory or folder, these list includes:
+1. The parent directory of the file
+2. The file name
+3. The size of the file
+4. The MD5 value of the file
+5. The SHA1 value of the file
 
 Basic syntax in command line
-> python listdir.py [Directory] [CSV File Name]
+> python listdir.py [Directory] [File Name]
 
 ```
 Note:
-  - Remove any succeeding backslash from the end of the directory
-  - You can prove any name for the csv file without adding the .csv extension
+  - Remove any succeeding backslash from the end of the directory to avoid any errors
 ```
--- listdir.py
+++ listdir.py
@@ -1,53 +1,78 @@
 from argparse import RawTextHelpFormatter
+import zipfile
+import hashlib
 import argparse
 import os.path
 import glob
 
-def get_file_name(dir_path_glob):
+def get_file_name(dir_path):
 
     """Getting the file name from path
     
     Arguments:
-        dir_path_glob -- Contains the path of the file
+        dir_path -- Contains the path of the file
     
     Returns:
         File name from the path
         
     """
 
-    split_path = dir_path_glob.split("\\")
+    split_path = dir_path.split("\\")
     return '"' + split_path[len(split_path) - 1] + '"'
 
-def get_dir_path(dir_path_glob):
+def get_dir_path(dir_path):
 
     """From glob's path, it replaces all double back slash to one forward slash
     
     Arguments:
-        dir_path_glob -- Contains the path of the file
+        dir_path -- Contains the path of the file
     
     Returns:
         Returns the whole path of the file with double quote marks
     """
 
-    replace_symb = dir_path_glob.replace("\\", "/")
+    replace_symb = dir_path.replace("\\", "/")
     split_path = replace_symb.split("/")
     split_path.pop()
     return '"' + os.path.realpath("/".join(split_path)) + '"'
 
-def get_file_size(dir_path_glob):
+def get_file_size(dir_path):
 
     """A function to get the file size
     
     Arguments:
-        dir_path_glob -- Contains the path of the file
+        dir_path -- Contains the path of the file
 
     Returns
         Returns the file size of the file using the getsize method from os.path
 
     """
 
-    file_realpath = os.path.realpath(dir_path_glob)
+    file_realpath = os.path.realpath(dir_path)
     return os.path.getsize(file_realpath)
+
+def get_file_hasher(dir_path):
+    
+    """Gets the hash value of a file in both MD5 and SHA1
+    
+    Arguments:
+        dir_path -- Contains the path of the file
+    
+    Returns:
+        Returns a list that contains the md5 and sha1 value of a file
+    """
+    
+    file_realpath = os.path.realpath(dir_path)
+    hasher_md5 = hashlib.md5()
+    hasher_sha1 = hashlib.sha1()
+    try:
+        with open(file_realpath, 'rb') as afile:
+            buf = afile.read()
+            hasher_md5.update(buf)
+            hasher_sha1.update(buf)
+    except Exception as e:
+        print(e)
+    return [hasher_md5.hexdigest(), hasher_sha1.hexdigest()]
 
 def export_csv(dir_path, csv_name):
 
@@ -71,9 +96,16 @@ def export_csv(dir_path, csv_name):
     try:
         with open(csv_name, "w") as new_file:
             file_list = []
+            file_list.append(f'File Directory,File Name,File Size,MD5,SHA1')
             for file_info in files:
-                file_list.append(f"{get_dir_path(file_info)},{get_file_name(file_info)},{get_file_size(file_info)}")
+                if os.path.isfile(file_info):
+                    file_list.append(f"{get_dir_path(file_info)},{get_file_name(file_info)},{get_file_size(file_info)},{get_file_hasher(file_info)[0]},{get_file_hasher(file_info)[1]}")
             new_file.write("\n".join(file_list))
+        
+        with zipfile.ZipFile(csv_name + '.zip', 'w') as zip_file:
+            zip_file.write(csv_name)
+            print("Success!")
+
     except Exception as e:
         return e
     return True
@@ -95,21 +127,12 @@ def check_valid_path(path):
     return True if os.path.isdir(real_path) or os.path.exists(real_path) else False
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Exports all file information in a CSV file of all files in a directory/folder.\n"
+    parser = argparse.ArgumentParser(description="Exports all file information into a file, it includes all files in a directory/folder.\n"
                                                             "Remove any succeeding back slash '\\' if it prints out any errors")
     parser.add_argument("directory", type=str, help="Full path of a folder")
-    parser.add_argument("file_name", type=str, help="CSV file name\nexample: file_name.csv")
+    parser.add_argument("file_name", type=str, help="File Name")
     user_inp = parser.parse_args()
     if check_valid_path(user_inp.directory):
-        destination_file = user_inp.file_name.split('.')
-        if len(destination_file) > 1:
-            if destination_file[len(destination_file) - 1] == 'csv':
-                export_csv(user_inp.directory, ".".join(destination_file))
-                print("Command Executed!")
-            else:
-                print("Invalid File name!")
-        else:
-            print("Command Executed!")
-            export_csv(user_inp.directory, destination_file[0] + '.csv')
+        export_csv(user_inp.directory, user_inp.file_name)
     else:
         print("Invalid path!")
